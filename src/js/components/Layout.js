1df8a5f25eca1c561bf22e34dd750a930842336f
nicole2112 <nicole.rodriguez@unitec.edu>
2021-09-06 10:06:42 -0600

    terminado tutorial 4

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,25 +1,32 @@
 import React from "react";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
-   constructor(){
-       super();
-       this.state={
-           title: "Welcome",
-       };
-   }
+    constructor(){
+        super();
+        this.state = {
+            title: "Welcome"
+        };
+    }
+
     render() {
+        /*
         setTimeout(() => {
             this.setState({title: "Welcome Will!"});
-        },2000);  
-        
+        }, 2000);
+
+
         return (
             <div>
                 <Header title={this.state.title}/>
                 <Header title={"Other Title"}/>
                 <Footer />
             </div>
+        );*/
+        return (
+            <h1>KillerNews.net</h1>
         );
     }
 }
