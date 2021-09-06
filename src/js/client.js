3c754bfd923c9a4bfa8dc6d8b93279d5d2ff51bd
mario rolando velasquez navarro <rolando.velasquez18@hotmail.com>
2021-09-05 19:58:16 -0600

    video3y4

--- src/js/client.js
+++ src/js/client.js
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Layout extends React.Component {
-    constructor(){
-        super();
-        this.name = "Will";
-    }
-
-    render() {
-        return (
-            <h1>It's working, {this.name}!</h1>
-        );
-    }
-}
+import Layout from "./components/Layout"
 
 const app = document.getElementById('app');
 
